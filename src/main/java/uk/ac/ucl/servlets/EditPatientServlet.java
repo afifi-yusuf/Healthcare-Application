73e6042c46f8bf4f4c96e5b3af4b171e20c72d06
Yusuf <yusuf.afifi@gmail.com>
2024-03-18 22:36:28 +0000

    added BaseWebServlet super class for oop

--- src/main/java/uk/ac/ucl/servlets/EditPatientServlet.java
+++ src/main/java/uk/ac/ucl/servlets/EditPatientServlet.java
@@ -1,32 +1,11 @@
 package uk.ac.ucl.servlets;
 
-import uk.ac.ucl.model.Model;
-import uk.ac.ucl.model.ModelFactory;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
-// The servlet invoked to perform a search.
-// The url http://localhost:8080/runsearch.html is mapped to calling doPost on the servlet object.
-// The servlet object is created automatically, you just provide the class.
 @WebServlet("/editPatient.html")
-public class EditPatientServlet extends HttpServlet
-{
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
-    {
-        // Invoke the JSP.
-        // A JSP page is actually converted into a Java class, so behind the scenes everything is Java.
-        ServletContext context = getServletContext();
-        RequestDispatcher dispatch = context.getRequestDispatcher("/editPatient.jsp");
-        dispatch.forward(request, response);
+public class EditPatientServlet extends BaseWebServlet {
+    @Override
+    protected String getJspPath() {
+        return "/editPatient.jsp";
     }
-
 }
--- src/main/java/uk/ac/ucl/servlets/IdFindServlet.java
+++ src/main/java/uk/ac/ucl/servlets/IdFindServlet.java
@@ -1,32 +1,11 @@
 package uk.ac.ucl.servlets;
 
-import uk.ac.ucl.model.Model;
-import uk.ac.ucl.model.ModelFactory;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
-// The servlet invoked to perform a search.
-// The url http://localhost:8080/runsearch.html is mapped to calling doPost on the servlet object.
-// The servlet object is created automatically, you just provide the class.
 @WebServlet("/idfind.html")
-public class IdFindServlet extends HttpServlet
-{
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-    {
-
-        // Invoke the JSP page.
-        ServletContext context = getServletContext();
-        RequestDispatcher dispatch = context.getRequestDispatcher("/idSearch.jsp");
-        dispatch.forward(request, response);
+public class IdFindServlet extends BaseWebServlet {
+    @Override
+    protected String getJspPath() {
+        return "/idSearch.jsp";
     }
-
 }
